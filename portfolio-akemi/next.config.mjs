c42887cd2bc3911257400a462c45d3554c6d72ca
Ana Paula Katsuda <a01025303@tec.mx>
2025-01-28 21:14:04 -0600

    Fixed next.config.mjs

--- portfolio-akemi/next.config.mjs
+++ portfolio-akemi/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/portfolio-akemi",
+    basePath: "/portfolio",
     output: "export", 
     reactStrictMode: true,
 };
