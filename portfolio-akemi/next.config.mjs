f22312493e7d8915b36e2ba089d0a927b9824862
Ana Paula Katsuda <a01025303@tec.mx>
2025-01-28 21:11:39 -0600

    First attempt pipeline

--- portfolio-akemi/next.config.mjs
+++ portfolio-akemi/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/portfolio-akemi",
+    output: "export", 
+    reactStrictMode: true,
+};
 
 export default nextConfig;
