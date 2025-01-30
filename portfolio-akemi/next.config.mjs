28abf36c1283ee02aefc6ee8dac7c2ea363a2a97
Ana Paula Katsuda <a01025303@tec.mx>
2025-01-29 19:22:58 -0600

    more deployment fixes 2

--- portfolio-akemi/next.config.mjs
+++ portfolio-akemi/next.config.mjs
@@ -2,10 +2,6 @@ const isProd = process.env.NODE_ENV === "production";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: isProd ? "/portfolio":"",
-    dev: "next dev",
-    build: "next build",
-    export: "next export",
-    start: "next start",
     output: "export", 
     reactStrictMode: true,
     images: {
